59619cfaa7c24a1148f8bb3e5f3c5f221ee4ff77
Aiub Duduev <aub.duduev.1989@gmail.com>
2023-03-25 09:19:08 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -4,32 +4,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "BannerAdvertising",
+    name: "ScreenAdvertising",
     platforms: [
         .iOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BannerAdvertising",
-            targets: ["BannerAdvertising"]),
+            name: "ScreenAdvertising",
+            targets: ["ScreenAdvertising"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/it-senior-developer/VVMLibrary", branch: "master"),
-        .package(url: "https://github.com/it-senior-developer/FirebaseBackend", branch: "master"),
-        .package(url: "https://github.com/it-senior-developer/AdvertisingAppsFlyer", branch: "master"),
+        .package(url: "https://github.com/GamesAdvertesings/Architecture", branch: "main"),
+        .package(url: "https://github.com/GamesAdvertesings/AdvertisingBackend", branch: "master"),
+        .package(url: "https://github.com/GamesAdvertesings/AdvertisingFlyer", branch: "master"),
         .package(url: "https://github.com/Juanpe/SkeletonView.git", from: "1.7.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "BannerAdvertising",
+            name: "ScreenAdvertising",
             dependencies: [
-                .product(name: "VVMLibrary", package: "VVMLibrary"),
-                .product(name: "FirebaseBackend", package: "FirebaseBackend"),
-                .product(name: "AdvertisingAppsFlyer", package: "AdvertisingAppsFlyer"),
+                .product(name: "Architecture", package: "Architecture"),
+                .product(name: "AdvertisingBackend", package: "AdvertisingBackend"),
+                .product(name: "AdvertisingFlyer", package: "AdvertisingFlyer"),
                 .product(name: "SkeletonView", package: "SkeletonView"),
             ]),
     ]
